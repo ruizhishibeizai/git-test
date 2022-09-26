df574135f4ed70144619682046597ed64004695f
Aatrox <335992583@qq.com>
2022-09-26 21:31:36 +0800

    first commit

---
+++ src/main/java/com/atguigu/git/GitTest.java
@@ -0,0 +1,12 @@
+package com.atguigu.git;
+
+/**
+ * @version 1.0
+ * @author: ZSZ
+ * @create: 2022-09-26 21:26
+ **/
+public class GitTest {
+    public static void main(String[] args) {
+        System.out.println("hello,git");
+    }
+}
