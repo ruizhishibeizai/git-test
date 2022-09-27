f069ee84ece6c0168c054358474af9e479ea4b8a
Aatrox <335992583@qq.com>
2022-09-27 13:22:36 +0800

    second commit

--- src/main/java/com/atguigu/git/GitTest.java
+++ src/main/java/com/atguigu/git/GitTest.java
@@ -8,5 +8,7 @@ package com.atguigu.git;
 public class GitTest {
     public static void main(String[] args) {
         System.out.println("hello,git");
+        System.out.println("忽略文件");
+        System.out.println("忽略文件");
     }
 }
